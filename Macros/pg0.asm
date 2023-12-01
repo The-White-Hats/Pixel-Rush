7d357e6df8b047eb4495096a84be579d8bf2f4d6
Adham-hussin <67987638+Adham-hussin@users.noreply.github.com>
2023-12-01 13:57:55 +0200

    ⭐All Validations Made

--- Macros/pg0.asm
+++ Macros/pg0.asm
@@ -1,10 +1,11 @@
-include usernames.inc
+include assets.inc
 .model small
 .stack 64
 .data
     mes1 db "Please enter your name:", 10, 13, 9, '$'
     mes2 db "Initial points:", 10, 13, 9, '$'
-    warning db "First character MUST be a letter",'$'
+    nameWarning db "First character MUST be a letter",'$'
+    pointsWarning db "Points MUST be a number",'$'
     name1 db 15, 0, 15 dup('$')
     points1 db 15, 0, 15 dup('$')
     name2 db 15, 0, 15 dup('$')
@@ -13,18 +14,22 @@ include usernames.inc
 main proc far
     mov ax, @data
     mov ds, ax
+    clear
     showmes mes1
     getstr name1
-    checkFirstChar name1, warning
+    checkFirstChar name1, nameWarning
     endl
     showmes mes2
     getstr points1
+    checkIfAllNumbers points1, pointsWarning
     clear
     showmes mes1
     getstr name2
-    checkFirstChar name2, warning
+    checkFirstChar name2, nameWarning
     endl
     showmes mes2
     getstr points2
+    checkIfAllNumbers points2, pointsWarning
+
 main endp
 end main
