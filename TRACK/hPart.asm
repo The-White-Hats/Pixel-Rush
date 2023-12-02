382d2dd4d8879029c2b6f8f094c5339bbe4e1bac
Adham-hussin <67987638+Adham-hussin@users.noreply.github.com>
2023-12-02 17:57:03 +0200

    ➕ Add Features Corners Validation

--- TRACK/hPart.asm
+++ TRACK/hPart.asm
@@ -1,5 +1,7 @@
 include hPart.inc
 include circSect.inc
+
+
 .model small
 .stack 64
 .data
@@ -16,7 +18,7 @@ include circSect.inc
          BLACK equ 00h
          DASHESCOLOR equ 07h
          ;----------------------------------Positions-------------------------------------------------;      
-         START_X dw 70
+         START_X dw 150
          START_Y dw 150
          END_X dw ?
          END_Y dw ?
@@ -31,14 +33,9 @@ include circSect.inc
          BOUNDARY_WIDTH equ 1
          BOUNDARY_LENGTH equ 4
          DASHEDLINE_LENGTH equ 6
-         RADIUS dw 20
-         p0x dw 0
-         p1x dw 0
-         p2x dw 0
-         p0y dw 0
-         p1y dw 0
-         p2y dw 0
-         direction db 1
+         cornerFlag db 0
+         cornerType db 1 ;1 means up, 0 means down
+         horizontalDirection db 0
 
 .code
 main proc far
@@ -47,22 +44,136 @@ main proc far
     mov ah, 0
     mov al, 13h
     int 10h
-    ;draw two horizontal roads with different directions
-    
-    horizontalPart START_X, START_Y, direction
 
-    mov ax, START_X
-    add ax, LINE_LENGTH
-    mov END_X, ax
-    mov ax, START_Y
-    add ax, LINE_WIDTH
-    add ax, 2*BOUNDARY_WIDTH
-    mov END_Y, ax
-    ;deletePart START_X, START_Y, END_X, END_Y
+    call checkHorizontalTrack
+    mov START_X, 170
+    call checkHorizontalTrack
+
     mov ah, 4ch        
     INT 21h         
 
 main endp
+
+CheckHorizontalTrack PROC
+	    mov bx,0
+		mov cx, START_X
+		mov dx, START_Y
+        ;---------------------------check for bounadry conditions---------------------------------;
+		    ;--------------------------------put the final position of dx and check with it---------------------------------;
+	        cmp horizontalDirection ,0
+				jnz down3
+				sub cx,LINE_LENGTH ;dx
+				jmp skip3
+				down3:
+				add cx,LINE_LENGTH ;dx
+				skip3:
+
+				cmp dx,MAX_Y
+				jle check1
+				jmp return
+            ;----------------------------------------------------------------------------------------------------------------;
+			check1: 
+				sub dx,LINE_WIDTH
+				sub dx,BOUNDARY_WIDTH*2
+				cmp dx,MIN_Y
+				jge check2
+				jmp return
+
+			check2:
+				cmp cx,MAX_X ;dx
+				jle check3
+				jmp return
+
+			check3:
+				cmp cx,MIN_X ;dx
+				jge checkcolor
+				jmp return 
+
+		checkcolor:
+		;-------------------------- horizontal line check = ---------------------------------;
+        mov bx,0
+        mov cx,START_X
+		checkHorizontal:
+		    mov dx,START_Y
+
+
+		    cmp bx,LINE_WIDTH
+			jz checkVertical_pre
+			inc bx ; inc counter untill reach the length of the line
+
+             ;-----------------------------update dx according to horizontalDirection -----------------------------------------;
+			cmp horizontalDirection ,0
+			jnz down1
+			dec cx
+			jmp skip1
+			down1:
+			inc cx
+			skip1:
+             ;----------------------------------------------------------------------------------------------------------------;
+			
+			mov ah,0dh
+			int 10h
+			cmp al,BLACK
+		jz checkHorizontalparallel ;if not green return
+		ret
+
+		checkHorizontalparallel:
+
+			add dx,LINE_WIDTH  
+			add dx,BOUNDARY_WIDTH*2 ;same y-axis but x-axis is increased by the width of the line and the boundary
+
+			mov ah,0dh
+			int 10h
+			cmp al,BLACK
+
+		jz checkHorizontal  ;if not green return
+		ret 
+        
+		;--------------------------vertical line check | | ---------------------------------;
+		checkVertical_pre:
+		mov bx,0
+        mov dx,START_Y
+		checkVertical:
+
+		    mov cx,START_X
+		    cmp bx,LINE_LENGTH
+			jz break
+			inc bx
+
+			dec dx
+
+			mov ah,0dh
+			int 10h
+			cmp al,BLACK
+
+		jz checkVerticalparallel
+		ret
+
+		checkVerticalparallel:
+
+			cmp horizontalDirection ,0
+			jnz down2
+			sub cx ,LINE_LENGTH
+			jmp skip2
+			down2:
+			add cx ,LINE_LENGTH
+			skip2:
+
+			mov ah,0dh
+			int 10h
+			cmp al,BLACK
+		jz checkVertical
+         ret 
+		 
+		 return:
+		 ret
+         break:
+		horizontalPart START_X, START_Y, horizontalDirection
+        ret
+checkHorizontalTrack ENDP
+
+
+
 end main
 
 
