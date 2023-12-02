b276ead4e12094a6ee06ba23c75f8e5d667b4d33
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2023-12-02 18:00:51 +0200

    Virtical Track Part

--- TRACK/Parts_3.asm
+++ TRACK/Parts_3.asm
@@ -16,8 +16,10 @@
 		 LIGHT_GRAY equ 07h
 		 GRAY equ 08h
          ;----------------------------------Positions-------------------------------------------------;      
-         START_X dw 10
-		 START_Y dw 150
+         START_X dw 0
+		 START_Y dw 30
+		 START_X2 dw 30
+		 START_Y2 dw 75
 		 ;----------------------------------BOUNDARY-------------------------------------------------;      
 		 MIN_X equ 0
 		 MAX_X equ 320
@@ -25,7 +27,7 @@
 		 MAX_Y equ 150 
          ;----------------------------------Dimentions-------------------------------------------------;
 		 LINE_WIDTH dw 20
-		 LINE_LENGTH dw 80
+		 LINE_LENGTH dw 60
 		 BOUNDARY_WIDTH equ 1
 		 BOUNDARY_LENGTH equ 4
 		 DASHEDLINE_LENGTH equ 6
@@ -38,6 +40,7 @@
 		 middletrackflag db 0
          verticaldirection db 0 ; 0 up 1 down
 		 Vertical_Track_Delete_Flag db 0
+
      	.CODE
 
 
@@ -55,7 +58,12 @@ MAIN 	PROC FAR
 		
 
 		;---------------------------------------Screen Coloring------------------------------------------------
-            xor cx,cx
+		; mov ax ,0600h
+		; mov bh,GREEN
+		; mov cx,0h
+		; mov dx , 184fh
+     	; int 10h
+		   xor cx,cx
 			xor dx,dx
 		    mov cl,0 ;x axis
 			mov dl,0 ;y axis
@@ -69,76 +77,149 @@ MAIN 	PROC FAR
 		  inc dl
 		  cmp dl,200
 		  jnz back
-		    
         ;------------------------------------Vertical Line-------------------------------------------;  
 			mov cx,START_X
 			mov dx,START_Y
 			mov posx,cx
 			mov posy,dx
-		    call GenerateVerticalTrack
-			; l1:
-			; jmp l1
-			; xor Vertical_Track_Delete_Flag,1
-			; mov cx,START_X
-			; mov dx,START_Y
+		   ; call  GenerateVerticalTrack 
+			; mov cx,START_X2
+			; mov dx,START_Y2
+			; mov START_X,cx
+			; mov START_Y,dx
 			; mov posx,cx
 			; mov posy,dx
-			;call GenerateVerticalTrack
-
-
+		    call  CheckVerticalTrack 
+			
         MOV AH, 4CH         ; Function to exit program
         INT 21H             ; Call DOS interrupt to exit
 
 MAIN ENDP
 
-GenerateVerticalTrack PROC 
 
+CheckVerticalTrack PROC
+	    mov bx,0
+		mov cx,START_X
+		mov dx,START_Y
+        ;---------------------------check for bounadry conditions---------------------------------;
+		    ;--------------------------------put the final position of dx and check with it---------------------------------;
+	        cmp verticaldirection ,0
+				jnz down3
+				sub dx,LINE_LENGTH
+				jmp skip3
+				down3:
+				add dx,LINE_LENGTH
+				skip3:
+
+				cmp cx,MIN_X
+				jge check1
+				jmp return
+            ;----------------------------------------------------------------------------------------------------------------;
+			check1: 
+				add cx,LINE_WIDTH
+				add cx,BOUNDARY_WIDTH*2
+				cmp cx,MAX_X
+				jle check2
+				jmp return
+
+			check2:
+				cmp dx,MIN_Y
+				jge check3
+				jmp return
+
+			check3:
+				cmp dx,MAX_Y
+				jle checkcolor
+				jmp return 
+
+		checkcolor:
+		;-------------------------- vertical line check | | ---------------------------------;
+        mov bx,0
+		mov dx,START_Y
+		checkvertical:
+		    mov cx,START_X
+
+		    cmp bx,LINE_LENGTH
+			jz checkhorizontal_pre
+			inc bx ; inc counter untill reach the length of the line
+
+             ;-----------------------------update dx according to verticaldirection -----------------------------------------;
+			cmp verticaldirection ,0
+			jnz down1
+			dec dx 
+			jmp skip1
+			down1:
+			inc dx
+			skip1:
+             ;----------------------------------------------------------------------------------------------------------------;
+			
+			mov ah,0dh
+			int 10h
+			cmp al,GREEN
+		jz checkverticalparallel ;if not green return
+		ret
+
+		checkverticalparallel:
+
+			add cx,LINE_WIDTH  
+			add cx,BOUNDARY_WIDTH*2 ;same y-axis but x-axis is increased by the width of the line and the boundary
+
+			mov ah,0dh
+			int 10h
+			cmp al,GREEN
+
+		jz checkvertical  ;if not green return
+		ret 
+        
+		;--------------------------horizontal line check = ---------------------------------;
+		checkhorizontal_pre:
+		mov bx,0
+		mov cx,START_X
+		checkhorizontal:
+
+		    mov dx,START_Y
+		    cmp bx,LINE_WIDTH
+			jz break
+			inc bx
+
+			inc cx
+
+			mov ah,0dh
+			int 10h
+			cmp al,GREEN
+
+		jz checkhorizontalparallel
+		ret
+
+		checkhorizontalparallel:
+
+			cmp verticaldirection ,0
+			jnz down2
+			sub dx ,LINE_LENGTH
+			jmp skip2
+			down2:
+			add dx ,LINE_LENGTH
+			skip2:
+
+			mov ah,0dh
+			int 10h
+			cmp al,GREEN
+		jz checkhorizontal
+         ret 
+		 
+		 return:
+		 ret
+         break:
+		call GenerateVerticalTrack
+        ret
+CheckVerticalTrack ENDP
+
+GenerateVerticalTrack PROC 
 		movehorizontal:      
 			mov boundaryflag ,  0
 			drawvertical:
-
 			    mov cx,posx
 				mov dx,posy
-
-                cmp Vertical_Track_Delete_Flag,1
-				jz mid31
-              
-				cmp posx,MAX_X
-				jnz check1
-				jmp delete
-				check1:
-				cmp posx,MIN_X
-				jnz check2
-				jmp delete
-				check2:
-				cmp posy,MIN_Y
-				jnz checkcolor
-				jmp delete
-				checkcolor:
-				mov ah,0dh
-				int 10h
-				cmp al,GREEN
-				jz draw
-                delete:
-				mov bx,posx
-                sub bx,START_X
-				mov LINE_WIDTH,bx
-				mov bx,posy
-				sub bx,START_Y
-
-				
-				mov LINE_LENGTH,bx
-                mov bx,START_X
-				mov posx,bx
-				mov bx,START_Y
-				mov posy,bx
-
-				mov Vertical_Track_Delete_Flag,1
-                call GenerateVerticalTrack
-				mov Vertical_Track_Delete_Flag,0
-				RET
-				draw:
-				;-----------------------checkfor delete flag if true just draw with green---------------------;
 				
                 ;------- if it is the first two lines then draw it with the red and white (boundary) else gray and lightgray with toggle-----;
 				mov bx,START_X
@@ -153,8 +234,6 @@ GenerateVerticalTrack PROC
 				jnz whitebounadrycolor 
 				mov al,RED
 				jmp boundarytogglecheck
-				mid31:
-				jmp mid3
 				whitebounadrycolor:
 				mov al,WHITE
                 boundarytogglecheck:
@@ -168,18 +247,16 @@ GenerateVerticalTrack PROC
 				xor boundaryflag,1
                 jmp next
 
-                mid3:
-				jmp  deleteverticaltrack
-              
-
 				roadcoloring:
                 mov bx,START_X
 				add bx,BOUNDARY_WIDTH
 				add bx,LINE_WIDTH
                 cmp posx,bx
                 jge boundarycoloring 
-                
-				 push ax
+
+                ;--------------- if it is not the middle track then jump to next ,equation  bx = START_X + BOUNDARY_WIDTH + LINE_WIDTH/2 ------------------------------;
+
+				push ax
                 mov ax,LINE_WIDTH
 				mov bx,2
 				div bl
@@ -190,28 +267,24 @@ GenerateVerticalTrack PROC
 				add bx,START_X
                 add bx,BOUNDARY_WIDTH
 
-                cmp posx,bx; if it is not the middle track then jump to next
-				jnz normaltrackcoloring
-		
-
-                cmp middletrackflag,0 ;Checl if i will color white incase of middle track
-				jnz normaltrackcoloring ; else normal coloring
-				mov al,WHITE
-				jmp middeltrackcheck
-
+                cmp posx,bx
                 
+				;-------------------------------------------------------------------------------------------------------------------------------------------------------;
+
+				jnz normaltrackcoloring
+                cmp middletrackflag,0   ;Checl if i will color white incase of middle track
+				jnz normaltrackcoloring ; else normal coloring
+				mov al,LIGHT_GRAY
+				jmp middeltrackcheck
 
 				mid4:
 				  jmp next
                
-
                 mid1:  ;for Far jump resoloution
 				jmp drawvertical
-
 				
 
 				normaltrackcoloring:
-
                 mov al,GRAY
 
                 middeltrackcheck:; if it is the middle track then check if i finished the dashed line length then toggle the flag
@@ -225,8 +298,8 @@ GenerateVerticalTrack PROC
 		
 				add bx,START_X
                 add bx,BOUNDARY_WIDTH
-
-				cmp posx,bx; if it is not the middle track then jump to next
+               
+				cmp posx,bx; if it is not the middle track then jump to next ,bx = START_X + BOUNDARY_WIDTH + LINE_WIDTH/2
 				jnz next
 
 				inc toggletrackmiddle
@@ -238,16 +311,13 @@ GenerateVerticalTrack PROC
 
                 mid2:
 				jmp movehorizontal
-                
-				deleteverticaltrack:
-				mov al,GREEN
 
 				next:
-
 				mov ah,0ch
 				int 10h
-
-				cmp verticaldirection ,0
+                
+				;---------------------------------------change posy according to vertical direction if up dec and cmp with start - length if down inc and cmp with start+length ----------------------------------------------;
+				cmp verticaldirection ,0 
 				jnz down  
 				dec posy
 				mov bx,START_Y
@@ -260,22 +330,22 @@ GenerateVerticalTrack PROC
 				add bx,LINE_LENGTH
 				cmp posy,bx
 				skip:
+                ;-------------------------------------------------------------------------------------------------------------------------------------------------------;
 
-            jnz mid1
+            jnz mid1 ;jmp to mid1 then to drawvertical if less  than the length
 
             mov bx,START_Y
             mov posy,bx
 
             inc posx
 
-			
-           mov bx,LINE_WIDTH
-		   add bx,BOUNDARY_WIDTH*2
-		   add bx,START_X
+			mov bx,LINE_WIDTH 
+			add bx,BOUNDARY_WIDTH*2
+			add bx,START_X
 
-			cmp posx,bx
+			cmp posx,bx ;bx = LINE_WIDTH  + START_X +BOUNDARY_WIDTH*2 ->total width of the line
 
-		 jnz mid2  ;jmp to again listen again for the next instruction
+		 jnz mid2  ;jmp to mid2 then to movehorizontal
 
 		RET
 	GenerateVerticalTrack endp
