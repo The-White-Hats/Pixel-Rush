bd92979c179f578fb91aaf56478119f88ea2e4f6
Adham-hussin <67987638+Adham-hussin@users.noreply.github.com>
2023-12-05 17:46:51 +0200

    ☠️ Random Update

--- TRACK/rndmv2.asm
+++ TRACK/rndmv2.asm
@@ -1,11 +1,8 @@
 .model small
 .stack 64
 .data
-seed DW 1234H       ; Initial seed value
-multiplier Dw 1d51H
-increment Dw 8c45H
-modulus DW 0F4bFh 
-random_seed db 0, 10, 13, '$' ; 4 bytes of memory to store the random seed
+seed dw 1234H       ; Initial seed value
+random db ?         ; Random number
 
 .code   
 main proc far
@@ -19,7 +16,7 @@ main proc far
     call rndm
     
     mov ah,09h
-    lea dx,random_seed
+    lea dx,random
     int 21h 
     ; call delay interrupt to delay 211 ms
     
@@ -33,40 +30,23 @@ main proc far
 main endp
 
 rndm proc
-    mov al, 0
-    mov ah, 86h
-    mov dx, 211
-    mov cx, 7
-    int 15h
-
     mov ah, 2Ch
-    int 21h 
-    mov ax, dx
-    mov bl, 17
-    mul bl
-    mov bl, 137
-    div bl
-    mov al,ah
-    mov ah, 17
-    mov bx, ax
+    int 21h
+    add seed, dx
+    mov ax, seed
+    mov bx, 32749
+    mul bx
+    add ax, 32541
+    mov seed, ax
 
-    
-    mov al,dl
-    mov bl, 5
-    mul bl
-
-    MOV BX, multiplier
-    MUL BX               ; AX = seed * multiplier
-    ADD AX, increment    ; AX = AX + increment
-    MOV CX, modulus      ; CX = modulus (2^16)
-    DIV CX               ; AX = AX / CX, DX = AX % CX (remainder)
+    ; Convert the result to a number between 0 and 11
     mov bh, 12
-    div bh
-    mov random_seed, ah
-    
+    div bh      ; Divide DX:AX by BL
+    mov random, ah
 
     ret
 rndm endp
+
 
 
 end main
