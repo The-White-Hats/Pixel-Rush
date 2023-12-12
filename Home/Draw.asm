011b03f069e3902f3f73b60cefbee061b55da395
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-12-12 16:59:32 +0200

    full drawing with procedures and interrupts

---
+++ Home/Draw.asm
@@ -0,0 +1,57 @@
+;AUTHOR : Ahmed Aladdin
+
+;---------------------------------------
+.286
+.MODEL SMALL
+.STACK 32
+;---------------------------------------
+.DATA
+
+filename1 db 'logo.bin', 0
+filename2 db 'th.bin', 0
+buffer_size equ 26624           ; the array size which is gonna be filled with the img data from the .bin file
+buffer db buffer_size dup(?)
+
+errtext db "YOUR ERROR MESSAGE", 10, "$"
+
+SCREEN_WIDTH equ 320
+SCREEN_HEIGHT equ 200
+
+Xstart dw ?
+Ystart dw ?
+Xlimit dw ?
+Ylimit dw ?
+
+errorFlag db 0
+;---------------------------------------
+.code
+include pm.inc
+
+MAIN PROC FAR
+    MOV AX,@DATA
+    MOV DS,AX
+
+    ; open graphics mode
+    mov ah,0
+    mov al,13h
+    int 10h
+
+    readImageM filename1
+
+    draw 100, 10, 120, 120
+
+    ; wait for a key input to not close the screen
+    MOV AH, 0
+    INT 16h
+
+    error_exit:
+    mov ah, 9
+    mov dx, offset errtext
+    int 21h
+
+    MOV AH,4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
+
