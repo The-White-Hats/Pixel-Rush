9c0cb1c85088069b94fb3982686af73649bad7e2
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-12-13 13:38:04 +0200

    take input phase one

--- Home/home.asm
+++ Home/home.asm
@@ -7,28 +7,43 @@
 ;---------------------------------------
 .DATA
 include LogoData.inc
+include userData.inc
 ;---------------------------------------
 .code
 include pm.inc
+include infoM.inc
 
 MAIN PROC FAR
     MOV AX,@DATA
     MOV DS,AX
+    mov es, ax
 
     ; open graphics mode
     UltraGraphicsMode
+    ; PaintScreen 06d, 0H, 0ffffh
 
     drawLogo
 
+    showColoredMsg mes1, 0fH, mes1size, mes1y, mes1x
+    MoveCursor nameInputX, nameInputY
+    ; mov bx, 15d
+    ; mov 
+    getstr name1
+    checkFirstChar name1, nameWarning
+    
+
+    
     ; wait for a key input to not close the screen
     MOV AH, 0
     INT 16h
+    jmp success
 
     error_exit:
     mov ah, 9
     mov dx, offset errtext
     int 21h
 
+    success:
     MOV AH,4CH
     INT 21H
 
