9dab3c2b5eee2d363fd8a34398127ed129e555e2
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-12-14 15:33:58 +0200

    clean the code

--- Home/home.asm
+++ Home/home.asm
@@ -20,45 +20,10 @@ MAIN PROC FAR
 
     ; open graphics mode
     UltraGraphicsMode
-
     paintAreaM 0, 0, 640, 480, 2AH  ; paint the screen in orange
-
     drawLogo
 
-    pusha
-    paintAreaM 212d, 325d, 216d, 50d, 0H  ; paint input area in black
-
-    readImageM firstPN
-    draw 220, 305, 199, 15
-
-    ; showColoredMsg mes1, 0fH, mes1size, mes1y, mes1x
-    MoveCursor nameInputX, nameInputY
-    getSizedStrM name1, 15d
-    checkFirstChar name1, nameWarning
-    paintAreaM 201, 305, 238, 17, 2AH   ; clear 'player 1 name' logo
-    popa
-
-    ; second player
-    pusha
-    readImageM secondPN
-    draw 220, 305, 199, 15
-    paintAreaM 212d, 325d, 216d, 50d, 0H  ; paint input area in black
-
-    ; showColoredMsg mes12, 0fH, mes1size, mes1y, mes1x
-    MoveCursor nameInputX, nameInputY
-    getSizedStrM name2, 15d
-    checkFirstChar name2, nameWarning
-
-    paintAreaM 210d, 325d, 220d, 50d, 2AH  ; clear the error area
-    paintAreaM 201, 305, 238, 17, 2AH 
-    paintAreaM 135, 464, 369, 11, 2AH 
-    popa
-
-    ; MoveCursor 34d, 25d
-    ; showmes name1
-    ; showchar ' '
-    ; showmes name2
-
+    GetPlayersNames
     
     ; wait for a key input to not close the screen
     MOV AH, 0
